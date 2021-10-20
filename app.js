15087bfded35e06e449558a82cc668872634c556
Vijay Raj <87898460+Vijayrajdev@users.noreply.github.com>
2021-10-20 22:15:35 +0530

    Update app. Js
    FEATURE : Updated velocity value.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const fetchSpaceStation = async () => {
   // contents
   document.getElementById('lat').innerHTML = (data.latitude).toFixed(3);
   document.getElementById('lon').innerHTML = (data.longitude).toFixed(3);
-  document.getElementById('vel').innerHTML = (data.velocity).toFixed(3);
+  document.getElementById('vel').innerHTML = (data.velocity).toFixed(0);
   // Getting latitude and logitude
   let { latitude, longitude } = data
   marker.setLatLng([latitude, longitude])
